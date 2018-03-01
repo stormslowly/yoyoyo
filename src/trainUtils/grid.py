569f29c1b28b09442d710fa32d3e0c361aed012a
stormslowly@gmail.com <stormslowly@gmail.com>
2018-03-01 18:40:26 +0800

    💄

--- src/trainUtils/grid.py
+++ src/trainUtils/grid.py
@@ -6,45 +6,69 @@ from PIL import Image, ImageDraw, ImageFont
 
 from PIL import Image
 
-image = Image.open('./BJ.jpg')
-btn_image = Image.open('./login_youke.png')
+image = Image.open('./BJ.jpg').convert("RGBA")
+btn_image = Image.open('./login_youke.png').convert("RGBA")
 
 
 def put_grid_on(image):
     image_draw = ImageDraw.Draw(image)
 
-    n_x_grid = 16
-    n_y_grid = 12
+    x_grid_size = 40
+    y_grid_size = 40
 
-    x_grid_size = 800 / n_x_grid
-    y_grid_size = 600 / n_y_grid
+    w, h = image.size
+
+    n_x_grid = w / x_grid_size
+    n_y_grid = h / y_grid_size
 
     for i in range(n_x_grid):
         x = (i + 1) * x_grid_size
-        image_draw.line((x, 0, x, 600), fill=0, width=2)
+        image_draw.line((x, 0, x, h), fill=0, width=4)
 
     for i in range(n_y_grid):
         y = (i + 1) * y_grid_size
-        image_draw.line((0, y, 800, y), fill=0, width=2)
+        image_draw.line((0, y, w, y), fill=0, width=4)
 
     return image
 
 
 scale_to = T.Compose(
     [
-        T.Scale((800, 600)),
+        # T.Scale((800, 600)),
         # T.ToTensor(),
         put_grid_on,
         # T.ToPILImage(),
     ]
 )
 
-image = scale_to(image).convert("RGBA")
 
-btn_image = btn_image.convert("RGBA")
+def paste_to(to_paste_img, pos, convas):
+    image_w, image_h = to_paste_img.size
+    x, y = pos
+    target_position = (x - image_w / 2, y - image_h / 2)
+    convas.paste(to_paste_img, target_position, to_paste_img)
 
-image.paste(btn_image)
+    return convas
 
+
+def draw_rectangle(image):
+    image_draw = ImageDraw.Draw(image)
+
+    im_w, im_h = image.size
+
+    center_x = im_w / 2
+    center_y = im_h / 2
+
+    image_draw.rectangle(xy=[(1, 1), (im_w - 2, im_h - 2)], outline=(255, 50, 0))
+    image_draw.ellipse(xy=[center_x - 4, center_y - 4, center_x + 4, center_y + 4],
+                       fill=(255, 0, 0))
+
+    return image
+
+
+paste_to(draw_rectangle(btn_image), (200, 200), image)
+
+image = scale_to(image)
 image.save('./BJ.grid.png')
 
 plt.imshow(image)
