dee0b6ff6332ac9919707c102cc3452262277597
stormslowly@gmail.com <stormslowly@gmail.com>
2018-03-13 23:42:24 +0800

    🎨 mult target

--- src/generate_train_data.py
+++ src/generate_train_data.py
@@ -1,5 +1,5 @@
 import os
-from random import choice, triangular
+from random import choice, triangular, randint
 
 from PIL import Image
 
@@ -47,21 +47,33 @@ class_list_dict = dict((fn, index) for (index, fn) in enumerate(class_list))
 class_images = [Image.open(os.path.join(CONFIG_DIR, fn)).convert('RGBA') for fn in class_list]
 
 for id in range(2000):
-    picked_class = class_list_dict[choice(class_list)]
-    picked_image = class_images[picked_class]
+    labels = []
 
-    w, h = [s * 1.0 / image_size for s in picked_image.size]
-
-    cx, cy = (triangular(0.5 * w, 1 - 0.5 * w), triangular(0.5 * h, 1 - 0.5 * h))
-
-    label = (picked_class, cx, cy, w, h)
-
+    n_objects = randint(1, 12)
     canvas = square_bg_image.copy()
 
-    canvas.paste(picked_image, bbox((cx * image_size, cy * image_size), picked_image), picked_image)
+    for n in range(n_objects):
+        picked_class = class_list_dict[choice(class_list)]
+        picked_image = class_images[picked_class]
+
+        scale = triangular(0.5, 0.8)
+
+        orig_size = picked_image.size
+
+        picked_image = picked_image.copy().resize((int(v * scale) for v in orig_size))
+
+        w, h = [s * 1.0 / image_size for s in picked_image.size]
+
+        cx, cy = (triangular(0.5 * w, 1 - 0.5 * w), triangular(0.5 * h, 1 - 0.5 * h))
+
+        label = (picked_class, cx, cy, w, h)
+
+        labels.append(label)
+
+        canvas.paste(picked_image, bbox((cx * image_size, cy * image_size), picked_image), picked_image)
 
     canvas.resize((512, 512)).save(os.path.join(TRAIN_DIR, '%05d.png' % (id + 1)))
 
     f = open(os.path.join(TRAIN_DIR, '%05d.txt' % (id + 1)), 'w')
     with f:
-        f.write(' '.join(str(v) for v in label))
+        f.write('\n'.join(' '.join(str(v) for v in l) for l in labels))
